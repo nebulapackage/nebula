40d69a15a5e16ca41b418045883ee8d980358ab9
Ryan Chandler <ryan@chandler.codes>
2020-10-09 17:20:41 +0100

    fix(manual registration): check if instance is passed before instantiation

--- src/Nebula.php
+++ src/Nebula.php
@@ -2,6 +2,10 @@
 
 namespace Larsklopstra\Nebula;
 
+use Larsklopstra\Nebula\Contracts\NebulaDashboard;
+use Larsklopstra\Nebula\Contracts\NebulaPage;
+use Larsklopstra\Nebula\Contracts\NebulaResource;
+
 class Nebula
 {
     protected static array $resources = [];
@@ -63,7 +67,7 @@ class Nebula
     public static function availableResources()
     {
         return collect(static::$resources)
-            ->map(fn ($resource) => new $resource)
+            ->map(fn ($resource) => $resource instanceof NebulaResource ? $resource : new $resource)
             ->merge(config('nebula.resources'))
             ->unique()
             ->toArray();
@@ -77,7 +81,7 @@ class Nebula
     public static function availableDashboards()
     {
         return collect(static::$dashboards)
-            ->map(fn ($dashboard) => new $dashboard)
+            ->map(fn ($dashboard) => $dashboard instanceof NebulaDashboard ? $dashboard : new $dashboard)
             ->merge(config('nebula.dashboards'))
             ->unique()
             ->toArray();
@@ -91,7 +95,7 @@ class Nebula
     public static function availablePages()
     {
         return collect(static::$pages)
-            ->map(fn ($page) => new $page)
+            ->map(fn ($page) => $page instanceof NebulaPage ? $page : new $page)
             ->merge(config('nebula.pages'))
             ->unique()
             ->toArray();
