9b57ec7307724257705be287e171354cb6f0d671
Johnny Fekete <hello@johnnyfekete.com>
2020-10-10 09:08:40 +0200

    clean up service provider

---
+++ src/Http/RouteResolvers/DashboardResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Larsklopstra\Nebula\Http\RouteResolvers;
+
+use Exception;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+class DashboardResolver
+{
+    /**
+     * Binds the "dashboard" route to the list of routes.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     * @throws HttpException
+     * @throws NotFoundHttpException
+     */
+    public static function bind(): void
+    {
+        Route::bind('dashboard', function ($value) {
+            $dashboards = config('nebula.dashboards');
+
+            if (empty($dashboards)) {
+                throw new Exception('No dashboards set in the nebula config.');
+            }
+
+            foreach ($dashboards as $dashboard) {
+                $dashboardExists = Str::of($dashboard->name())
+                    ->lower()
+                    ->is($value);
+
+                if ($dashboardExists) {
+                    return $dashboard;
+                }
+            }
+
+            throw new Exception("Dashboard $value not found.");
+        });
+    }
+}
---
+++ src/Http/RouteResolvers/PageResolver.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Larsklopstra\Nebula\Http\RouteResolvers;
+
+use Exception;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Larsklopstra\Nebula\Nebula;
+
+class PageResolver
+{
+    /**
+     * Binds the page routes to the list of routes.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     * @throws HttpException
+     * @throws NotFoundHttpException
+     */
+    public function bind(): void
+    {
+        Route::bind('page', function ($value) {
+            $pages = Nebula::availablePages();
+
+            if (empty($pages)) {
+                throw new Exception('No pages set in the nebula config.');
+            }
+
+            foreach ($pages as $page) {
+                $pageExists = Str::of($page->slug())
+                    ->lower()
+                    ->is($value);
+
+                if ($pageExists) {
+                    return $page;
+                }
+            }
+
+            throw new Exception("Page {$page} not found.");
+        });
+    }
+}
