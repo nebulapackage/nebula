c38ea970d9481d09b7f1efc068782b47d8964845
Foks <37844753+FoksVHox@users.noreply.github.com>
2020-11-20 18:44:08 +0100

    Update Textarea.php

--- src/Fields/Textarea.php
+++ src/Fields/Textarea.php
@@ -6,7 +6,7 @@ use Larsklopstra\Nebula\Contracts\NebulaField;
 use Larsklopstra\Nebula\Fields\Concerns\HasHelperText;
 use Larsklopstra\Nebula\Fields\Concerns\HasPlaceholder;
 
-class Textarea extends NebulaField
+class TextareaField extends NebulaField
 {
     use HasPlaceholder, HasHelperText;
 }
